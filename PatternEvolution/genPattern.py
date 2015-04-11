667b3c143fbc87748277809176c1ba15f4ea310f
Serena Booth <sbooth@college.harvard.edu>
2015-04-11 13:52:24 -0400

    Updating evolutionary algorithm

--- PatternEvolution/genPattern.py
+++ PatternEvolution/genPattern.py
@@ -15,42 +15,54 @@
 
 import random
 
-import numpy
+import numpy as np
 
 from deap import algorithms
 from deap import base
 from deap import creator
 from deap import tools
+from _functools import partial
+
+def genRandomImage():
+    im_rep = np.zeros((NUM_PIXELS,NUM_PIXELS,3), int)
+
+    i = 0 
+    while (i < 0.5 * NUM_PIXELS * NUM_PIXELS):
+        x = random.randint(0,NUM_PIXELS -1)
+        y = random.randint(0,NUM_PIXELS -1)
+        if (im_rep[x][y][0] == 0):
+            im_rep[x][y] = np.array([1,1,1])
+            i = i + 1
+
+    return im_rep
+
+NUM_PIXELS = 100 
+POPULATION = 40
+NGEN = 100
+PIC = genRandomImage()
+
+def gen_one_random_pixel(): 
+    color = random.randint(0,1) # returns 0 or 1 for b or w 
+    return tuple([random.randint(0, NUM_PIXELS - 1), random.randint(0, NUM_PIXELS - 1), color])
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", numpy.ndarray, fitness=creator.FitnessMax)
+creator.create("Individual", np.ndarray, fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
 
-toolbox.register("attr_bool", random.randint, 0, 1)
+toolbox.register("attr_bool", gen_one_random_pixel)
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=100)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 # SERENA: evalPattern should compute invoke OpenCV optic flow call; should be difference between real motion and calculated OF? Should penalize (badly) for all single color image. We're maximizing the difference between the calculated movement and computed OF diff. 
-def evalOneMax(individual):
-    return sum(individual),
+def evalOneMax(individual, pixles_to_add):
+    for tri in pixles_to_add:
+        individual[tri[0], tri[1]] = tri[2]
+    return 100, 
+    #return individual,
 
 # SERENA: as we use numpy.ndarray representation, this should work in a similar way for cxTwoPatterns
 def cxTwoPointCopy(ind1, ind2):
-    """Execute a two points crossover with copy on the input individuals. The
-    copy is required because the slicing in numpy returns a view of the data,
-    which leads to a self overwritting in the swap operation. It prevents
-    ::
-    
-        >>> import numpy
-        >>> a = numpy.array((1,2,3,4))
-        >>> b = numpy.array((5.6.7.8))
-        >>> a[1:3], b[1:3] = b[1:3], a[1:3]
-        >>> print(a)
-        [1 6 7 4]
-        >>> print(b)
-        [5 6 7 8]
-    """
     size = len(ind1)
     cxpoint1 = random.randint(1, size)
     cxpoint2 = random.randint(1, size - 1)
@@ -65,13 +77,13 @@ def cxTwoPointCopy(ind1, ind2):
     return ind1, ind2
     
     
-toolbox.register("evaluate", evalOneMax)
+toolbox.register("evaluate", partial(evalOneMax, PIC))
 toolbox.register("mate", cxTwoPointCopy)
 toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 def main():
-    random.seed(64)
+    #random.seed(64)
     
     pop = toolbox.population(n=300)
     
@@ -79,13 +91,13 @@ def main():
     # equality element wise, which raises an exception in the if similar()
     # check of the hall of fame. Using a different equality function like
     # numpy.array_equal or numpy.allclose solve this issue.
-    hof = tools.HallOfFame(1, similar=numpy.array_equal)
+    hof = tools.HallOfFame(1, similar=np.array_equal)
     
     stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean)
-    stats.register("std", numpy.std)
-    stats.register("min", numpy.min)
-    stats.register("max", numpy.max)
+    stats.register("avg", np.mean)
+    stats.register("std", np.std)
+    stats.register("min", np.min)
+    stats.register("max", np.max)
     
     algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=40, stats=stats,
                         halloffame=hof)
